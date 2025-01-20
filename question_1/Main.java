f3cb3c34ba4001a8da4afe87afd093a720fa687c
Matheus <matheus.goncalves@gamese.gg>
2025-01-20 16:49:24 -0300

    Resolução do primeiro exercício

---
+++ question_1/Main.java
@@ -0,0 +1,14 @@
+package question_1;
+
+public class Main {
+    public static void main(String[] args) {
+        int INDICE = 13, SOMA = 0, K = 0;
+
+        while (K < INDICE) {
+            K = K + 1;
+            SOMA = SOMA + K;
+        }
+
+        System.out.println("SOMA = " + SOMA); // O resultado é 91
+    }
+}
